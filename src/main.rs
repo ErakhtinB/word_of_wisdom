d3d7a8ce1be2ad30d7db08da3d264af1f1606558
boris <boris.erakhtin@gmail.com>
2022-03-21 10:26:52 +0300

    Server source code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(async_closure)]
+use anyhow::Result;
+use std::sync::Arc;
+use tcpserver::{Builder, IPeer, ITCPServer};
+use tokio::io::AsyncReadExt;
+use hashcash::check_with_params;
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+use std::str;
+
+const QUOTES: &'static [&'static str] = &[
+    "Quote One",
+    "Quote Two",
+    "Quote Two",
+];
+
+fn get_random_quote() -> &'static str {
+    let mut rng = rand::thread_rng();
+    return QUOTES[rng.gen_range(0..(QUOTES.len() - 1))];
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let tcpserver: Arc<dyn ITCPServer<()>> = Builder::new("0.0.0.0:5555")
+        .set_connect_event(|_addr| {
+            return true;
+        })
+        .set_stream_init(async move |tcp_stream| {
+            Ok(tcp_stream)
+        })
+        .set_input_event(async move |mut reader, peer, _token| {
+            let mut buff = [0; 4096];
+            let mut on_verification = false;
+            let rand_string: String = thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(32)
+                .map(char::from)
+                .collect();
+            while let Ok(len) = reader.read(&mut buff).await {
+                if len == 0 {
+                    break;
+                }
+                if !on_verification {
+                    peer.send_ref(rand_string.as_bytes()).await?;
+                    on_verification = true;
+                } else {
+                    match check_with_params(&str::from_utf8(&buff[..len].to_vec()).unwrap(),
+                        Some(&rand_string), None, None) {
+                        Ok(valid) => {
+                            if valid {
+                                peer.send_ref(&get_random_quote().as_bytes()).await?;
+                            } else {
+                                println!("Stamp invalid");
+                            }
+                        }
+                        Err(e) => {
+                            println!("{}", e);
+                        }
+                    }
+                    on_verification = false;
+                }
+            }
+            Ok(())
+        })
+        .build()
+        .await;
+    tcpserver.start_block(()).await?;
+    Ok(())
 }
